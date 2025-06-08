c400ff5c5704dd94c94a53f420cd31f7fbb257ea
OmarFazli <mdomar.fazli@gmail.com>
2025-06-08 19:56:24 -0400

    added geodb api calls and implemented autocomplete async paginate

---
+++ src/apis/geodbcities.js
@@ -0,0 +1,9 @@
+export const GEODB_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const  GEODB_API_OPTIONS = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'e57fe85a32msh5344a0eda6db9cap18e33ajsn6ba99de6c28f',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
--- src/components/citysearch.js
+++ src/components/citysearch.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { AsyncPaginate } from "react-select-async-paginate"
+import { GEODB_API_URL, GEODB_API_OPTIONS } from "../apis/geodbcities"
 
 function CitySearch({onSearchChange}){
     const [search, setSearch] = useState(null)
@@ -8,12 +9,30 @@ function CitySearch({onSearchChange}){
         setSearch(searchValue);
         onSearchChange(searchValue);
     }
+
+    const loadOptions = (searchValue) => {
+        return fetch(`${GEODB_API_URL}/cities?minPopulation=500000&namePrefix=${searchValue}`, GEODB_API_OPTIONS)
+        .then((response) => response.json())
+        .then((response) => {
+            return {
+                options: response.data.map((city) => {
+                    return{
+                        value: `${city.latitude} ${city.longitude}`,
+                        label: `${city.name}, ${city.countryCode}`
+                    };
+                })
+            };
+        })
+        .catch((err) => console.log(err))
+    }
+
     return (
         <AsyncPaginate
             placeholder="Enter city"
             debounceTimeout={600}
             value={search}
             onChange={handleOnChange}
+            loadOptions={loadOptions}
         />
     )
 }
